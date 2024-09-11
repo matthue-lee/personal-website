a3222cee6a7172545bb4d5e81f4aed623b79cc32
Matthew Lee <tansenmatt@gmail.com>
2024-09-12 09:04:18 +1200

    fuKCING APOSTROPEHS

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -150,7 +150,7 @@ export default function Example() {
             type="submit"
             className="block w-full rounded-md bg-emerald-800 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            Let&#39;s talk
           </button>
         </div>
       </form>
