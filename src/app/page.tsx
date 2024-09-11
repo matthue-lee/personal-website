019bb07ef07140286decf8ac01cd525313b29d47
Matthew Lee <tansenmatt@gmail.com>
2024-09-11 14:29:30 +1200

    minor tweaks to layout of homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
 
        {/* Small Cards */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-20 mt-40 mb-8 w-[80%]">
-          <a href="/target-page-2" className="relative block bg-emerald-950 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+          <a href="/spectral-analysis" className="relative block bg-emerald-950 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
             <div className="p-16 border-4 border-emerald-950 rounded-lg overflow-hidden">
             <img className="w-full h-auto rounded-xl" src="/content/sbm.jpg" alt="Card Image"/>
             </div>
@@ -65,7 +65,7 @@ export default function Home() {
               </div>
             </div>
           </a>
-          <a href="/target-page-3" className="relative block bg-emerald-950 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+          <a href="/underactuated-gripper" className="relative block bg-emerald-950 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
           <div className="p-16 border-4 border-emerald-950 rounded-lg overflow-hidden">
             <img className="w-full h-auto rounded-xl" src="/content/gipper 1.jpg" alt="Card Image"
             style={{ maxHeight: '300px' }}/>
@@ -83,7 +83,7 @@ export default function Home() {
           </a>
           <a href="/holography" className="relative block bg-emerald-950 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
           <div className="p-16 border-4 border-emerald-950 rounded-lg overflow-hidden">
-            <img className="w-full h-auto rounded-xl" src="/content/p4p.png" alt="Card Image"/>
+            <img className="w-full h-auto rounded-xl mt-5" src="/content/p4p.png" alt="Card Image"/>
             </div>
             <div className="absolute top-0 left-0 right-0">
               <div className="p-3 md:p-5">
@@ -96,9 +96,9 @@ export default function Home() {
               </div>
             </div>
           </a>
-          <a href="/target-page-5" className="relative block bg-emerald-950 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+          <a href="/surfboard-rack" className="relative block bg-emerald-950 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
           <div className="p-16 border-4 border-emerald-950 rounded-lg overflow-hidden">
-            <img className="w-full h-auto rounded-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Card Image"/>
+            <img className="w-full h-auto mt-5 rounded-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Card Image"/>
           </div>
             <div className="absolute top-0 left-0 right-0">
               <div className="p-3 md:p-5">
--- src/app/surfboard-rack/page.js
+++ src/app/surfboard-rack/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      More details coming soon...
+      More details coming soon
     </div>
   )
 }
