d3e46c9beb628431270e202b72e9fbafb4b461a5
Matthew Lee <tansenmatt@gmail.com>
2024-09-25 11:16:43 +1200

    removed card bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     >
       <div className="text-xl md:text-3xl lg:text-4xl flex justify-start p-5 md:p-24 text-gray-950">
         {/* Hi there, I&#39;m Matthew Lee, BE(Hons)/BSc(Computer Science).<br /> */}
-        Please take a look at some of my work below.
+        A mixture of design and software projects I'm proud of.
       </div>
       <div className="flex-grow flex flex-col items-center">
         {/* Large Card */}
@@ -46,69 +46,49 @@ export default function Home() {
 
 
        {/* Small Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-20 mt-40 mb-8 w-[80%]">
-          <a href="/spectral-analysis" className="relative block bg-stone-200 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
-            <div className="p-16 border-4 border-stone-200 rounded-lg overflow-hidden">
-            <img className="w-full h-auto rounded-xl" src="/content/sbm.jpg" alt="Card Image"/>
-            </div>
-            <div className="absolute top-0 left-0 right-0">
-              <div className="p-3 md:p-5">
-                <h3 className="text-lg font-bold text-additional">
-                  Spectral Analysis
-                </h3>
-                <p className="mt-1 text-additional">
-                  A foray into machine learning and SVM&#39;s
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/underactuated-gripper" className="relative block bg-stone-200 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
-          <div className="p-20 border-4 border-stone-200 rounded-lg overflow-hidden">
-            <img className="w-full h-auto rounded-xl" src="/content/full assembly.png" alt="Card Image"
-            style={{ maxHeight: '300px' }}/>
-          </div>
-            <div className="absolute top-0 left-0 right-0">
-              <div className="p-3 md:p-5">
-                <h3 className="text-lg font-bold text-additional">
-                  Underactuated Gripper
-                </h3>
-                <p className="mt-1 text-additional">
-                  Biomechanical analsysis and top of the class anthropomorphic design
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/holography" className="relative block bg-stone-200 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
-          <div className="p-20 border-4 border-stone-200 rounded-lg overflow-hidden">
-            <img className="w-full h-auto rounded-xl mt-5" src="/content/p4p.png" alt="Card Image"/>
-            </div>
-            <div className="absolute top-0 left-0 right-0">
-              <div className="p-3 md:p-5">
-                <h3 className="text-lg font-bold text-additional">
-                  Holographic Imaging
-                </h3>
-                <p className="mt-1 text-additional">
-                  A numerical algorithm for imaging using Gabor in-line holography
-                </p>
-              </div>
-            </div>
-          </a>
-          <a href="/surfboard-rack" className="relative block bg-stone-200 border shadow-sm rounded-xl p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70 cursor-pointer transform transition-transform duration-300 hover:scale-105">
-          <div className="p-16 border-4 border-stone-200 rounded-lg overflow-hidden">
-            <img className="w-full h-auto mt-5 rounded-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Card Image"/>
-          </div>
-            <div className="absolute top-0 left-0 right-0">
-              <div className="p-3 md:p-5">
-                <h3 className="text-lg font-bold text-additional">
-                  Surfboard Rack
-                </h3>
-                <p className="mt-1 text-additional">
-                  A real test of my design skills, and a chance to practice CAD
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
+       <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-20 mt-40 mb-8 w-[80%]">
+  <a href="/spectral-analysis" className="block rounded-xl p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+    <div className="flex flex-col items-start">
+      <img className="w-full h-auto rounded-xl" src="/content/sbm.jpg" alt="Card Image" />
+      <div className="mt-4">
+        <h3 className="text-lg font-bold text-additional">Spectral Analysis</h3>
+        <p className="mt-1 text-additional">A foray into machine learning and SVM&#39;s</p>
+      </div>
+    </div>
+  </a>
+
+  <a href="/underactuated-gripper" className="block rounded-xl p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+    <div className="flex flex-col items-start">
+      <img className="w-full h-auto rounded-xl" src="/content/full assembly.png" alt="Card Image" style={{ maxHeight: '300px' }} />
+      <div className="mt-4">
+        <h3 className="text-lg font-bold text-additional">Underactuated Gripper</h3>
+        <p className="mt-1 text-additional">Biomechanical analysis and top of the class anthropomorphic design</p>
+      </div>
+    </div>
+  </a>
+
+  <a href="/holography" className="block rounded-xl p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+    <div className="flex flex-col items-start">
+      <img className="w-full h-auto rounded-xl mt-5" src="/content/p4p.png" alt="Card Image" />
+      <div className="mt-4">
+        <h3 className="text-lg font-bold text-additional">Holographic Imaging</h3>
+        <p className="mt-1 text-additional">A numerical algorithm for imaging using Gabor in-line holography</p>
+      </div>
+    </div>
+  </a>
+
+  <a href="/surfboard-rack" className="block rounded-xl p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+    <div className="flex flex-col items-start">
+      <img className="w-full h-auto mt-5 rounded-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Card Image" />
+      <div className="mt-4">
+        <h3 className="text-lg font-bold text-additional">Surfboard Rack</h3>
+        <p className="mt-1 text-additional">A real test of my design skills, and a chance to practice CAD</p>
+      </div>
+    </div>
+  </a>
+</div>
+
+
       </div>
     </main>
 );
